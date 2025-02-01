ce6a21e9441bb5230a9abf8665a150725f75f174
Theo Teske <theo.teske@gmail.com>
2025-01-31 22:00:20 -0600

    implemented local video processing with ffmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,42 @@
 import express from "express";
 
 const app = express();
-const port = 3000; // default for express
+app.use(express.json());
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 // simple GET endpoint to print "Hello, World!"
 app.get("/", (req, res) => {
     res.send("Hello World!")
 });
+
+// POST endpoint to process video locally
+app.post("/process-video", (req, res) => {
+    // get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    // error handling, check if the above are undefined
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully.");
+        })
+        .on("error", (err: Error) => {
+            console.log(`An error occurred: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
+});
+
+// set port
+const port = process.env.PORT || 3000; // 3000 is default for express
 
 // start server and listen on the port specified above for requests
 app.listen(port, () => {
