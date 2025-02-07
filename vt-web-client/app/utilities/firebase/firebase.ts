33b310544ff2bf4f743cfc9fb3cd02ed72c081f5
Theo Teske <theo.teske@gmail.com>
2025-02-07 00:57:01 -0600

    created firebase.ts and sign in and sign out

---
+++ vt-web-client/app/utilities/firebase/firebase.ts
@@ -0,0 +1,40 @@
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User,
+    UserCredential
+} from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXSXeKy14XCKJoLwtUyuxB_b_fBD4GhLQ",
+  authDomain: "viewtube-e2b83.firebaseapp.com",
+  projectId: "viewtube-e2b83",
+  appId: "1:265437101019:web:e5aee949aa70fbb38f05fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs in the user with a Google popup.
+ * 
+ * @returns A Promise that resolves when the user is signed in with the user's credentials.
+ */
+export function signInWithGoogle(): Promise<UserCredential> {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs out the user.
+ * 
+ * @returns A Promise that resolves when the user is signed out.
+ */
+export function signOut(): Promise<void> {
+    return auth.signOut();
+}
